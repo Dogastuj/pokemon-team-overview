535d4e9a49cb64693e9e5120a76c383d37a345ed
Dogastuj <lenourichel.nathan@gmail.com>
2025-04-04 18:15:47 +0200

    creation of the route my teams

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PokepasteComponent } from './pokepaste/pokepaste-component';
+import { MyTeamsComponent } from './my-teams/my-teams.component';
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
     {path: 'team-overview', component: PokepasteComponent},
+    {path: 'my-teams', component: MyTeamsComponent},
 ];
---
+++ src/app/my-teams/my-teams.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-teams',
+  imports: [],
+  templateUrl: './my-teams.component.html',
+  styleUrl: './my-teams.component.scss'
+})
+export class MyTeamsComponent {
+
+}
