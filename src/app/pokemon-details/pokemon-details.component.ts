f2ec2aed497dcdd2e309593012de365c978e9c49
Dogastuj <lenourichel.nathan@gmail.com>
2025-04-10 22:43:21 +0200

    creation of the component

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import { Component, Input } from '@angular/core';
 import { Pokemon } from '../models/pokemon.model';
+=======
+import { Component } from '@angular/core';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-pokemon-details',
@@ -8,6 +12,10 @@ import { Pokemon } from '../models/pokemon.model';
   styleUrl: './pokemon-details.component.scss'
 })
 export class PokemonDetailsComponent {
+<<<<<<< Updated upstream
   
   @Input() pokemon!: Pokemon;
+=======
+
+>>>>>>> Stashed changes
 }
